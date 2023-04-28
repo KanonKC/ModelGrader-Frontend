6525b9879b83da21a8e00661ba22fdd48cbe2937
KanonKC <kanonkce@gmail.com>
2023-04-28 21:58:10 +0700

    Remove dotenv

--- src/services/constant.service.js
+++ src/services/constant.service.js
@@ -1,7 +1,4 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-
-export const URL = REACT_APP_BACKEND_URL;
+export const URL = process.env.REACT_APP_BACKEND_URL;
 export const FORM_HEADER = {
 	headers: {
 		"content-type": "multipart/form-data",
