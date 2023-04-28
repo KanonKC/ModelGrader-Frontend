ce2b8af1ef23c6ebe71842b0ed94f8ec8a6dd111
KanonKC <kanonkce@gmail.com>
2023-04-28 21:26:10 +0700

    Env file

--- src/services/constant.service.js
+++ src/services/constant.service.js
@@ -1,4 +1,7 @@
-export const URL = "http://localhost:8000";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const URL = REACT_APP_BACKEND_URL;
 export const FORM_HEADER = {
 	headers: {
 		"content-type": "multipart/form-data",
--- src/views/AllProblem.js
+++ src/views/AllProblem.js
@@ -16,150 +16,185 @@ import ProblemsTable from "../components/ProblemsTable";
 import { AuthContext } from "../App";
 
 const columns = [
-    {
-        name: "Title",
-        minWidth: "200px",
-        selector: (row) => row.title,
-        sortable: true,
-    },
-    {
-        name: "Language",
-        maxWidth: "130px",
-        selector: (row) => Language[row.language],
-        sortable: true,
-    },
+	{
+		name: "Title",
+		minWidth: "200px",
+		selector: (row) => row.title,
+		sortable: true,
+	},
+	{
+		name: "Language",
+		maxWidth: "130px",
+		selector: (row) => Language[row.language],
+		sortable: true,
+	},
 
-    {
-        name: "Status",
-        center: true,
-        maxWidth: "100px",
-        selector: (row) => row.pass_status,
-        sortable: false,
-    },
+	{
+		name: "Status",
+		center: true,
+		maxWidth: "100px",
+		selector: (row) => row.pass_status,
+		sortable: false,
+	},
 
-    {
-        name: "Score",
-        center: true,
-        maxWidth: "50px",  
-        selector: (row) => row.best_submission_score,
-        sortable: false,
-    },
+	{
+		name: "Score",
+		center: true,
+		maxWidth: "50px",
+		selector: (row) => row.best_submission_score,
+		sortable: false,
+	},
 
-    {
-        name: "Best Submission",
-        center: true,
-        selector: (row) => <span className="font-mono">{row.best_submission_result}</span>,
-        sortable: false,
-    },
+	{
+		name: "Best Submission",
+		center: true,
+		selector: (row) => (
+			<span className="font-mono">{row.best_submission_result}</span>
+		),
+		sortable: false,
+	},
 
+	{
+		name: "Created By",
+		maxWidth: "130px",
+		selector: (row) => row.creator.username,
+		sortable: true,
+	},
 
-    {
-        name: "Created By",
-        maxWidth: "130px",
-        selector: (row) => row.creator.username,
-        sortable: true,
-    },
-
-    {
-        name: "",
-        selector: (row) => row.solve_button,
-    },
+	{
+		name: "",
+		selector: (row) => row.solve_button,
+	},
 ];
 
 const AllProblem = () => {
-    const nevigate = useNavigate()
-    const account_id = Number(localStorage.getItem("account_id"))
-    const [isLoggin, setisLoggin] = useContext(AuthContext)
+	const nevigate = useNavigate();
+	const account_id = Number(localStorage.getItem("account_id"));
+	const [isLoggin, setisLoggin] = useContext(AuthContext);
 
-    const [allProblems, setAllProblems] = useState([]);
-    const [allSubmissions, setallSubmissions] = useState([]);
-    const [displayProblems, setdisplayProblems] = useState([]);
+	const [allProblems, setAllProblems] = useState([]);
+	const [allSubmissions, setallSubmissions] = useState([]);
+	const [displayProblems, setdisplayProblems] = useState([]);
 
-    const [search, setsearch] = useState("");
+	const [search, setsearch] = useState("");
 
-    useEffect(() => {
-        getAllProblems().then((response) => {
-            setAllProblems(response.data.result);
-        });
-    }, []);
+	useEffect(() => {
+		getAllProblems().then((response) => {
+			setAllProblems(response.data.result);
+		});
+	}, []);
 
-    useEffect(() => {
-        getAuthorization().then((response) => {
-            setisLoggin(response.data.result);
-        });
-    }, [allProblems]);
+	useEffect(() => {
+		getAuthorization().then((response) => {
+			setisLoggin(response.data.result);
+		});
+	}, [allProblems]);
 
-    useEffect(() => {
-        if (isLoggin && account_id) {
-            viewAllSubmissions({
-                account_id: account_id,
-                sort_score: 1,
-            }).then((response) => {
-                setallSubmissions(response.data.result);
-            });
-        }
-    }, [account_id, isLoggin]);
+	useEffect(() => {
+		if (isLoggin && account_id) {
+			viewAllSubmissions({
+				account_id: account_id,
+				sort_score: 1,
+			}).then((response) => {
+				setallSubmissions(response.data.result);
+			});
+		}
+	}, [account_id, isLoggin]);
 
-    useEffect(() => {
-        try {
-            const filterProblems = allProblems.filter(
-                (problem) =>
-                    (hasSubstring(problem.title, search) ||
-                    hasSubstring(problem.language, search) ||
-                    hasSubstring(problem.creator.username, search)) && (!problem.is_private)
-            );
-            setdisplayProblems(
-                filterProblems.map((problem) => {
-                    let best_submission = allSubmissions.filter(
-                        (submission) =>
-                            submission.problem_id === problem.problem_id
-                    )[0];
-                    return {
-                        ...problem,
-                        best_submission_result: best_submission
-                            ? best_submission.result
-                            : "",
-                        best_submission_score: best_submission
-                            ? `${best_submission.score}/${best_submission.result.length}`
-                            : "",
-                        solve_button: (<div>
-                            <div className="hidden 2xl:block">
-                                <Button onClick={() => nevigate(`/problems/${problem.problem_id}`)} className="text-white" color="success"><FontAwesomeIcon icon={faPuzzlePiece}/> Solve This Problem</Button>
-                            </div>
-                            <div className="2xl:hidden">
-                                <Button onClick={() => nevigate(`/problems/${problem.problem_id}`)} className="text-white" color="success"><FontAwesomeIcon icon={faPuzzlePiece}/> Solve</Button>
-                            </div>
-                        </div>),
-                        pass_status: best_submission ? (best_submission.is_passed ? <img src={require("../imgs/passed_icon.png")} /> : <img src={require("../imgs/unpassed_icon.png")} />) : ""
-                    };
-                })
-            );
-        } catch (err) { }
-    }, [allProblems, allSubmissions, search, nevigate]);
+	useEffect(() => {
+		try {
+			const filterProblems = allProblems.filter(
+				(problem) =>
+					(hasSubstring(problem.title, search) ||
+						hasSubstring(problem.language, search) ||
+						hasSubstring(problem.creator.username, search)) &&
+					!problem.is_private
+			);
+			setdisplayProblems(
+				filterProblems.map((problem) => {
+					let best_submission = allSubmissions.filter(
+						(submission) =>
+							submission.problem_id === problem.problem_id
+					)[0];
+					return {
+						...problem,
+						best_submission_result: best_submission
+							? best_submission.result
+							: "",
+						best_submission_score: best_submission
+							? `${best_submission.score}/${best_submission.result.length}`
+							: "",
+						solve_button: (
+							<div>
+								<div className="hidden 2xl:block">
+									<Button
+										onClick={() =>
+											nevigate(
+												`/problems/${problem.problem_id}`
+											)
+										}
+										className="text-white"
+										color="success"
+									>
+										<FontAwesomeIcon icon={faPuzzlePiece} />{" "}
+										Solve This Problem
+									</Button>
+								</div>
+								<div className="2xl:hidden">
+									<Button
+										onClick={() =>
+											nevigate(
+												`/problems/${problem.problem_id}`
+											)
+										}
+										className="text-white"
+										color="success"
+									>
+										<FontAwesomeIcon icon={faPuzzlePiece} />{" "}
+										Solve
+									</Button>
+								</div>
+							</div>
+						),
+						pass_status: best_submission ? (
+							best_submission.is_passed ? (
+								<img src={require("../imgs/passed_icon.png")} />
+							) : (
+								<img
+									src={require("../imgs/unpassed_icon.png")}
+								/>
+							)
+						) : (
+							""
+						),
+					};
+				})
+			);
+		} catch (err) {}
+	}, [allProblems, allSubmissions, search, nevigate]);
 
-    return (
-        <div className="pt-10 md:pt-24">
-            <Row className="">
-                <Col >
-                    <h1>All Problem</h1>
-                </Col>
-                <Col xs={4}>
-                    <SearchBar
-                        value={search}
-                        onChange={(e) => setsearch(e.target.value)}
-                    />
-                </Col>
+	return (
+		<div className="pt-10 md:pt-24">
+			<Row className="">
+				<Col>
+					<h1>All Problem</h1>
+				</Col>
+				<Col xs={4}>
+					<SearchBar
+						value={search}
+						onChange={(e) => setsearch(e.target.value)}
+					/>
+				</Col>
+			</Row>
 
-            </Row>
-
-            <div className="problem-card-list">
-                <ProblemsTable
-                    problems={displayProblems}
-                    submissions={allSubmissions}
-                />
-            </div>
-        </div>
-    );
+			<div className="problem-card-list">
+				<ProblemsTable
+					problems={displayProblems}
+					submissions={allSubmissions}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default AllProblem;
